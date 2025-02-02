fe055434d0acc5e7809496d6889e526802ac8257
bartektomczyk <bartlomiejtomczyk@gmail.com>
2025-02-02 17:14:52 +0100

    add linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FlatCompat } from '@eslint/eslintrc';
 
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
@@ -10,7 +13,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:react/recommended',
+    'airbnb',
+  ),
+  ...compat.plugins('jsx-a11y'),
+  ...compat.config({
+    rules: {
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    },
+  }),
 ];
 
 export default eslintConfig;
