0e3857e5ba7a922f2354a3e5e800f5d99ce2df0f
bartektomczyk <bartlomiejtomczyk@gmail.com>
2025-02-06 18:04:23 +0100

    add octokit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+// 'use client';
+
 import React from 'react';
 import {
   Button, Center, Container, HStack,
 } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({
+  auth: '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',
+});
+const fetchRepositories = await octokit.request('GET /search/repositories?q="cool"', {
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28',
+  },
+});
 
 export default function Home() {
+  // const { data, error } = useQuery({
+  //   queryFn: fetchRepositories,
+  //   queryKey: ['repositories'],
+  // });
+  //
+  // console.log('data', data);
+  // console.log('error', error);
+
+  console.log(fetchRepositories);
+
   return (
     <Center>
       <Container maxW="lg" fluid>
